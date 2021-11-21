e262272a8e9661faf1f072d112e599ea0aace881
Andrey Borodulin <borodulin@gmail.com>
2021-11-21 15:11:10 +0500

    init

--- src/DependencyInjection/ApiExtension.php
+++ src/DependencyInjection/ApiExtension.php
@@ -6,13 +6,10 @@ namespace Borodulin\Bundle\GridApiBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
-use Symfony\Component\Yaml\Exception\ParseException;
-use Symfony\Component\Yaml\Parser;
 
-class ApiExtension extends ConfigurableExtension implements PrependExtensionInterface
+class ApiExtension extends ConfigurableExtension
 {
     /**
      * Configures the passed container according to the merged configuration.
@@ -24,23 +21,5 @@ class ApiExtension extends ConfigurableExtension implements PrependExtensionInte
             new FileLocator(__DIR__ . '/../../config')
         );
         $loader->load('services.yml');
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        $yamlParser = new Parser();
-
-        try {
-            $doctrineConfig = $yamlParser->parse(
-                file_get_contents(__DIR__ . '/../../config/doctrine.yml')
-            );
-        } catch (ParseException $e) {
-            throw new \InvalidArgumentException(sprintf(
-                'The file "%s" does not contain valid YAML.',
-                __DIR__ . '/../../config/doctrine.yml'
-            ), 0, $e);
-        }
-
-        $container->prependExtensionConfig('doctrine', $doctrineConfig['doctrine']);
     }
 }
