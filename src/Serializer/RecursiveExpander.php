1f8918fa7e803bdc2cdcecbc33fe57888537be90
Andrey Borodulin <borodulin@gmail.com>
2021-11-23 21:05:35 +0500

    fix expand

--- src/Serializer/RecursiveExpander.php
+++ src/Serializer/RecursiveExpander.php
@@ -59,7 +59,7 @@ class RecursiveExpander
                 $expandTree[$key] = [];
             }
         }
-        $expand = array_filter($expand, fn ($item) => false !== strpos($item, '*'));
+        $expand = array_filter($expand, fn ($item) => false === strpos($item, '*'));
         foreach ($expand as $expandItem) {
             $normalizedNames = array_map(
                 fn ($item) => $nameConverter->normalize($item),
