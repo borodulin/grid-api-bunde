66a3eeccfb4c641f958e76d3242cf94a11dc1854
Andrey Borodulin <borodulin@gmail.com>
2021-11-23 23:15:12 +0500

    fix compiler pass

--- src/DependencyInjection/EntityConverterFactoryPass.php
+++ src/DependencyInjection/EntityConverterFactoryPass.php
@@ -29,19 +29,24 @@ class EntityConverterFactoryPass implements CompilerPassInterface
                 $reflection = new \ReflectionClass($class);
                 if ($reflection->implementsInterface(EntityConverterInterface::class)) {
                     /* @var EntityConverterInterface|string $class */
-                    if ($reflection->hasMethod('__invoke')) {
+                    try {
                         $method = $reflection->getMethod('__invoke');
-                        if (0 === $method->getNumberOfRequiredParameters()) {
-                            throw new \RuntimeException(sprintf('Invalid converter handler: method "%s::__invoke()" requires at least one argument, first one being the object it handles.', $reflection->getName()));
-                        }
-                        $parameters = $method->getParameters();
-                        if (!$type = $parameters[0]->getType()) {
-                            throw new \RuntimeException(sprintf('Invalid converter handler: argument "$%s" of method "%s::__invoke()" must have a type-hint corresponding to the object class it handles.', $parameters[0]->getName(), $reflection->getName()));
-                        }
-                        $converters[$type] = new Reference($class);
-                    } else {
-                        throw new \RuntimeException(sprintf('Invalid converter handler: method "%s::__invoke()" should be implemented. ', $reflection->getName()));
+                    } catch (\ReflectionException $e) {
+                        throw new \RuntimeException(sprintf('Invalid converter handler: class "%s" must have an "__invoke()" method.', $reflection->getName()));
                     }
+                    if (0 === $method->getNumberOfRequiredParameters()) {
+                        throw new \RuntimeException(sprintf('Invalid converter handler: method "%s::__invoke()" requires at least one argument, first one being the object it handles.', $reflection->getName()));
+                    }
+                    $parameters = $method->getParameters();
+                    if (!$type = $parameters[0]->getType()) {
+                        throw new \RuntimeException(sprintf('Invalid converter handler: argument "$%s" of method "%s::__invoke()" must have a type-hint corresponding to the object class it handles.', $parameters[0]->getName(), $reflection->getName()));
+                    }
+
+                    if ($type->isBuiltin()) {
+                        throw new \RuntimeException(sprintf('Invalid converter handler: type-hint of argument "$%s" in method "%s::__invoke()" must be a class , "%s" given.', $parameters[0]->getName(), $reflection->getName(), (string) $type));
+                    }
+
+                    $converters[(string) $type] = new Reference((string) $type);
                 }
             }
         }
