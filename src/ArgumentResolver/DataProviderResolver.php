5093162b5c3ddb572e4bbe378e3c60845252f2e9
Andrey Borodulin <borodulin@gmail.com>
2022-03-22 19:26:02 +0500

    refactoring

--- src/ArgumentResolver/DataProviderResolver.php
+++ src/ArgumentResolver/DataProviderResolver.php
@@ -11,34 +11,34 @@ use Borodulin\Bundle\GridApiBundle\GridApi\Expand\ExpandFactory;
 use Borodulin\Bundle\GridApiBundle\GridApi\Filter\FilterFactory;
 use Borodulin\Bundle\GridApiBundle\GridApi\Pagination\PaginationFactory;
 use Borodulin\Bundle\GridApiBundle\GridApi\Sort\SortFactory;
-use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver\ServiceValueResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
 class DataProviderResolver implements ArgumentValueResolverInterface
 {
-    private ContainerInterface $container;
     private SortFactory $sortRequestFactory;
     private ExpandFactory $expandRequestFactory;
     private FilterFactory $filterRequestFactory;
     private PaginationFactory $paginationRequestFactory;
     private ScenarioInterface $scenario;
+    private ServiceValueResolver $serviceValueResolver;
 
     public function __construct(
-        ContainerInterface $container,
         ScenarioInterface $scenario,
         SortFactory $sortRequestFactory,
         ExpandFactory $expandRequestFactory,
         FilterFactory $filterRequestFactory,
-        PaginationFactory $paginationRequestFactory
+        PaginationFactory $paginationRequestFactory,
+        ServiceValueResolver $serviceValueResolver
     ) {
-        $this->container = $container;
         $this->sortRequestFactory = $sortRequestFactory;
         $this->expandRequestFactory = $expandRequestFactory;
         $this->filterRequestFactory = $filterRequestFactory;
         $this->paginationRequestFactory = $paginationRequestFactory;
         $this->scenario = $scenario;
+        $this->serviceValueResolver = $serviceValueResolver;
     }
 
     public function supports(Request $request, ArgumentMetadata $argument): bool
@@ -51,16 +51,19 @@ class DataProviderResolver implements ArgumentValueResolverInterface
 
         $reflection = new \ReflectionClass($type);
 
-        return $reflection->isInstantiable()
+        return $this->serviceValueResolver->supports($request, $argument)
             && $reflection->implementsInterface(DataProviderInterface::class);
     }
 
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        $service = $this->container->get($argument->getType());
+        $service = null;
+        foreach ($this->serviceValueResolver->resolve($request, $argument) as $item) {
+            $service = $item;
+        }
         if ($service instanceof DataProviderDecorator) {
             yield $service;
-        } else {
+        } elseif ($service instanceof DataProviderInterface) {
             $sort = $this->sortRequestFactory->tryCreateFromInputBug($request->query);
             $expand = $this->expandRequestFactory->tryCreateFromInputBug($request->query);
             $pagination = $this->paginationRequestFactory->createFromInputBug($request->query);
@@ -73,6 +76,8 @@ class DataProviderResolver implements ArgumentValueResolverInterface
                 ->setFilter($filter)
                 ->setPagination($pagination)
             ;
+        } else {
+            throw new \InvalidArgumentException();
         }
     }
 }
