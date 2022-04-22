aae11f63c6fffc6a2f00cbe47d9aec9a2c496ae5
Andrey Borodulin <borodulin@gmail.com>
2022-04-22 23:52:31 +0500

    fix tags

--- src/DependencyInjection/ApiExtension.php
+++ src/DependencyInjection/ApiExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Borodulin\Bundle\GridApiBundle\DependencyInjection;
 
+use Borodulin\Bundle\GridApiBundle\EntityConverter\EntityConverterInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -16,6 +17,9 @@ class ApiExtension extends ConfigurableExtension
      */
     protected function loadInternal(array $mergedConfig, ContainerBuilder $container): void
     {
+        $container->registerForAutoconfiguration(EntityConverterInterface::class)
+            ->addTag('grid_api.entity_converter');
+
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../../config')
